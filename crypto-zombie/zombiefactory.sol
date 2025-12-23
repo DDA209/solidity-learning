80bd05e39ffb5b5b153c09b1a4c8d2233d8de996
DDA209 <didier.pascarel1@gmail.com>
2025-12-23 12:21:18 +0100

    olki9/8*

---
+++ crypto-zombie/zombiefactory.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: cryptozombie 
+// Version de Solidity à utiliser >, >=, =, <=, < peuvent être utilisés
+pragma solidity ^0.8.33;
+
+// Import de ownable
+import "./ownable.sol";
+
+// Déclaration du contrat
+// "is" permet l'héritage de "ownable" dans "ZombieFactory", ici nous avons accès aux éléments disponibles dans ownable
+contract ZombieFactory is Ownable {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) internal {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
+        _createZombie(_name, randDna);
+    }
+
+}
